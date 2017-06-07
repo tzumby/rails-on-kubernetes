aed750a7c08eb1b8be14e105d252e3bc7bb5b25e
Razvan <tzumby@gmail.com>
2017-06-07 11:10:16 -0400

    Updates on the rails app side

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq'
 
   devise_for :users
+
+  root to: sign_in_path
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
