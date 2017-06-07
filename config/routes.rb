a941655e657980e3744c594b42136817a075b888
Razvan <tzumby@gmail.com>
2017-06-07 11:23:32 -0400

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  root to: "devise/sessions#new"
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
