83f9acb5ac20264a228bb47b27ae0c28a4596ed0
Razvan <tzumby@gmail.com>
2017-06-07 11:14:12 -0400

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  root to: sign_in_path
+  root to: "devise/sessions#new"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
