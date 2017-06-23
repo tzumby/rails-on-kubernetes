4635725e1bd0edee4845d57330b32eb05ae7c306
Razvan <tzumby@gmail.com>
2017-06-23 10:00:22 -0400

    Added default_url_options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.action_mailer.default_url_options = { :host => 'rails.local' }
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
